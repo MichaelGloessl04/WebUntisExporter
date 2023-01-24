2ac9e2cda40bb33333e597c254714c81758c7557
Michael Glössl <gloess190117@gmail.com>
2023-01-24 18:47:54 +0100

    Implemented test for class Teachers tested by 00.07

--- tests/test_databank/test_import_00.py
+++ tests/test_databank/test_import_00.py
@@ -1,4 +1,5 @@
 def test_import_databank_00():
+    """Test if the databank module can be importet."""
     try:
         import databank  # noqa: F401
         assert True
@@ -7,6 +8,7 @@ def test_import_databank_00():
 
 
 def test_import_model_02():
+    """Test if the model module can be importet."""
     try:
         import databank.model  # noqa: F401
         assert True
@@ -15,6 +17,7 @@ def test_import_model_02():
 
 
 def test_import_absences_02():
+    """Test if the absences class can be importet."""
     try:
         from databank.model import Absences  # noqa: F401
         assert True
@@ -23,6 +26,7 @@ def test_import_absences_02():
 
 
 def test_import_subjects_03():
+    """Test if the subjects class can be importet."""
     try:
         from databank.model import Subjects  # noqa: F401
         assert True
@@ -31,6 +35,7 @@ def test_import_subjects_03():
 
 
 def test_import_teachers_04():
+    """Test if the teacher class can be importet."""
     try:
         from databank.model import Teachers  # noqa: F401
         assert True
@@ -39,6 +44,7 @@ def test_import_teachers_04():
 
 
 def test_import_todos_05():
+    """Test if the todos class can be importet."""
     try:
         from databank.model import Todos  # noqa: F401
         assert True
@@ -47,8 +53,18 @@ def test_import_todos_05():
 
 
 def test_import_users_06():
+    """Test if the users class can be importet."""
     try:
         from databank.model import Users  # noqa: F401
+        assert True
+    except ModuleNotFoundError:
+        assert False
+
+
+def test_import_timetable_07():
+    """Test if the timetable class can be importet."""
+    try:
+        from databank.model import Timetable  # noqa: F401
         assert True
     except ModuleNotFoundError:
         assert False
