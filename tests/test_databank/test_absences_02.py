84dd8384a5d18a492ac494fbe16c99f2621e190a
MichaelGloessl04 <55692241+MichaelGloessl04@users.noreply.github.com>
2023-02-09 14:17:41 +0100

    Implemented test for class::method Absences::delete tested by [02.07]

--- tests/test_databank/test_absences_02.py
+++ tests/test_databank/test_absences_02.py
@@ -50,7 +50,7 @@ def test_init_path_invalid_02():
 
 def test_init_funcitonality_03():
     """Test if the init method works as intended."""
-    a = AbsenceHandler()  # noqa: F841
+    a = AbsenceHandler(db_path)  # noqa: F841
 
 
 def test_Model_04():
@@ -89,3 +89,18 @@ def test_append_06():
         result = session.execute("""select * from %s """ % a.Model.__tablename__)
         assert expected == result.fetchall()
         a.delete(column="id", value="1")
+
+
+def test_delete_07():
+    """Test the delete method."""
+    expected = []
+    a = AbsenceHandler(db_path)  # noqa: F841
+    a.delete_all()
+    assert a.delete
+    a.append(start=temp_time, end=temp_time)
+    a.delete("id", "1")
+
+    with a.db_connection.connect() as session:
+        result = session.execute("""select * from %s """ % a.Model.__tablename__)
+        assert expected == result.fetchall()
+        a.delete_all()
