10f1d49e7391cc6a4ebf888b702e5cf858ec2d7e
MichaelGloessl04 <55692241+MichaelGloessl04@users.noreply.github.com>
2023-02-10 12:25:41 +0100

    bitte 3?

--- GUI/gui.py
+++ GUI/gui.py
@@ -6,6 +6,7 @@ from datetime import datetime
 class GUI:
     def __init__(self) -> None:
         self.window = tk.Tk()
+        self.window.geometry('350x200')
         t = databank.model.TimetableHandler("C:/Code/WebUntisExporter/databank/databank.db")
         s = databank.model.SubjectHandler("C:/Code/WebUntisExporter/databank/databank.db")
         table = t.To_Dict
@@ -17,7 +18,11 @@ class GUI:
         scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
         listbox = tk.Listbox(self.window, yscrollcommand=scrollbar.set)
-
+        for i in range(len(table['id'])):
+            label_text = f"""{s.get_where("short_name", table['subject'][i])[0][0]},
+                             {datetime.fromtimestamp(table['start'][i])},
+                             {datetime.fromtimestamp(table['end'][i])}"""
+            listbox.insert(tk.END, label_text)
         listbox.pack(fill=tk.BOTH)
 
         scrollbar.config(command=listbox.yview)
@@ -25,11 +30,5 @@ class GUI:
         for i in range(5):
             btn = tk.Button(self.window, text=weekday_labels[i])
             weekday_buttons.append(btn)
-
-        for i in range(len(table['id'])):
-            label_text = f"""{s.get_where("short_name", table['subject'][i])[0][0]},
-                             {datetime.fromtimestamp(table['start'][i])},
-                             {datetime.fromtimestamp(table['end'][i])}"""
-            listbox.insert(tk.END, tk.Label(self.window, text=label_text).pack())
 
         self.window.mainloop()
